1b80d6e822e2e01f39eb72d0b7ad9e89e632b0bb
ibicheema121 <152516698+ibicheema121@users.noreply.github.com>
2026-05-14 20:32:32 +0500

    Fix import statement for DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import DATABASE_URL
+from config import DATABASE_URL
 
 # Create SQLAlchemy engine
 engine = create_engine(
