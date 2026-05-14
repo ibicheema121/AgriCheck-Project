cb34f85fed149f9081724d16344a1542a26af211
ibicheema121 <152516698+ibicheema121@users.noreply.github.com>
2026-05-14 21:42:47 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
