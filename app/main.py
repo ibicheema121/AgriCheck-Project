f90c3299537fbe169b070430d8767344f3d91dba
ibicheema121 <152516698+ibicheema121@users.noreply.github.com>
2026-05-15 17:08:20 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
